a26049ee6982de1c3994532456cc400a99aa81d1
zdravko7 <zbotushanov@gmail.com>
2024-03-22 14:19:39 +0200

    test

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 
 import weather_controller
 
+weather_conditions = weather_controller.print_current_temperature()
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return weather_controller.print_current_temperature()
+    return "Home Page"
 
 @app.route('/contact', methods=['GET'])
 def contact():
