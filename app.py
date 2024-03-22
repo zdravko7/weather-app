29260f74cec99adcda7abdcd4597d26717028a74
zdravko7 <zbotushanov@gmail.com>
2024-03-22 14:25:50 +0200

    test4

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 # app.py
 
-import weather_controller
+import requests
+
+url = r"https://api.open-meteo.com/v1/forecast"
+
+params = {
+    "latitude": 42.69,
+    "longitude": 23.32,
+    "current": "temperature_2m,wind_speed_10m",
+
+
+}
+
+response = requests.get(url, params=params)
+
+responseJSON = response.json()
+
+def print_current_temperature():
+    return f'The current temperature is {str(responseJSON['current']['temperature_2m'])}'
+
+
+
+#import weather_controller
 
 #weather_conditions = weather_controller.print_current_temperature()
 
@@ -10,7 +31,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Home Page"
+    return print_current_temperature()
 
 @app.route('/contact', methods=['GET'])
 def contact():
